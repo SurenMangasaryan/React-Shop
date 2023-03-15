aaa6fd3495ca151d9b7008bbb087aaa2cd107a92
SurenMangasaryan <suromangasaryan@gmail.com>
2023-03-16 00:02:23 +0400

    react-shop

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router'
-import Header from '../Components/Header/Header'
+import Header from '../Header/Header'
 
 export default function Layout() {
     return (
