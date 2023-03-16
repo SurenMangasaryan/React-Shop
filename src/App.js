d4501cc9e25305e304937c7ef00f4a030c714395
SurenMangasaryan <suromangasaryan@gmail.com>
2023-03-17 01:17:07 +0400

    react-shop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 import Context from './Context'
 import Container from '../src/Components/Container/Container'
 import './App.css'
+import ScrollTop from './ScrollTop'
+
 
 export default function App() {
     return (
         <BrowserRouter>
             <Context>
+                <ScrollTop />
                 <Container />
             </Context>
         </BrowserRouter>
