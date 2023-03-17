ab53665f5403b9a1e8af63a1c7235a8f16e7a0e4
SurenMangasaryan <suromangasaryan@gmail.com>
2023-03-17 20:38:16 +0400

    react-shop

--- src/Components/HomePage/HomePageComponents/About.jsx
+++ src/Components/HomePage/HomePageComponents/About.jsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
+import { useNavigate, useParams } from 'react-router'
 
 export default function About() {
     const { id } = useParams();
     const [product, setProduct] = useState({});
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get(`https://fakestoreapi.com/products/${id}`)
@@ -13,9 +14,10 @@ export default function About() {
     }, [id])
 
     return (
-        <div >
+        <div className='about'>
             <h3 className='not-found not-found1'>{product.data !== undefined ? product.data.title : null}</h3>
             <h3 className='not-found not-found2'>{product.data !== undefined ? product.data.description : null}</h3>
+            <button className='btn-login' onClick={() => navigate(-1)}>Go back</button>
         </div>
     )
 }
